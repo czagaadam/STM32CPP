1eaf2ebc497c56899ad91e2f3a7bf6dd9ccba6be
czagaadam <czagaadam@gmail.com>
2024-08-29 19:57:29 +0200

    Remove comments

--- Samples/CPP_AND_C/Core/Src/main.cpp
+++ Samples/CPP_AND_C/Core/Src/main.cpp
@@ -1,22 +1,11 @@
-/* USER CODE BEGIN Header */
-/**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2024 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
-/* USER CODE END Header */
-/* Includes ------------------------------------------------------------------*/
+/*
+ * main.cpp
+ *
+ *  Author: Adam Czaga czagaadam@gmail.com
+ *
+ *	main.cpp for CPP_AND_C
+ *
+ */
 #include "main.h"
 #include "i2c.h"
 #include "gpio.h"
@@ -27,15 +16,13 @@
 #include <string.h>
 
 #ifdef __cplusplus
-extern "C"
-{
+extern "C" {
 #include "Inc/vl53l0x.h"
 #include "Inc/vl53l0x/vl53l0x_platform.h" //typedef VL53L0X_Dev_t
 #include "Inc/vl53l0x/vl53l0x_api.h"
 #include "Inc/vl53l0xHandlerClass.h"
 }
 #endif
-
 
 Dout Led1;
 Din Btn1;
@@ -59,36 +46,26 @@ void Btn1_call_back(void) {
 	Led1.write(GPIO_PIN_SET);
 }
 
-void I2C3_EV_IRQHandler(void)
-{
+void I2C3_EV_IRQHandler(void) {
 	HAL_I2C_EV_IRQHandler(&hi2c3);
 }
 
-void HAL_I2C_EV_IRQHandler()
-{
+void HAL_I2C_EV_IRQHandler() {
 
 }
 
-void HAL_I2C_MasterTxCpltCallback(I2C_HandleTypeDef *hi2c)
-{
+void HAL_I2C_MasterTxCpltCallback(I2C_HandleTypeDef *hi2c) {
 	char ConvertBuffer2[30] = "";
-	if(hi2c->Devaddress == 0x92)
-	{
+	if (hi2c->Devaddress == 0x92) {
 		sprintf(ConvertBuffer2, "Tenp sensor callback");
 		UART_SendString(ConvertBuffer2);
-	}
-	else if(hi2c->Devaddress == 0x40)
-	{
+	} else if (hi2c->Devaddress == 0x40) {
 		sprintf(ConvertBuffer2, "Port exp. callback");
 		UART_SendString(ConvertBuffer2);
-	}
-	else if(hi2c->Devaddress == 0x9A)
-	{
+	} else if (hi2c->Devaddress == 0x9A) {
 		sprintf(ConvertBuffer2, "ADC callback");
 		UART_SendString(ConvertBuffer2);
-	}
-	else
-	{
+	} else {
 		sprintf(ConvertBuffer2, "Unknown dev. callback");
 		UART_SendString(ConvertBuffer2);
 	}
@@ -132,9 +109,9 @@ void SystemClock_Config(void);
 /* USER CODE END 0 */
 
 /**
-  * @brief  The application entry point.
-  * @retval int
-  */
+ * @brief  The application entry point.
+ * @retval int
+ */
 int main(void) {
 
 	/* USER CODE BEGIN 1 */
@@ -145,12 +122,8 @@ int main(void) {
 
 	/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
 
-	/***************************************************/
-	/********************C Code*************************/
+	//GPIOD->BSRR = (uint32_t)GPIO_PIN_15 << 16U;
 	GPIOD->BSRR = (uint32_t) GPIO_PIN_14 << 16U;
-	/********************End of C Code******************/
-	/***************************************************/
-
 	HAL_Init();
 
 	/* USER CODE BEGIN Init */
@@ -174,47 +147,35 @@ int main(void) {
 	Btn1 = Din(GPIOD, GPIO_PIN_10);
 	Btn1.set_isr_cb(Btn1_call_back);
 
-	/***************************************************/
-	/********************C Code*************************/
-
 	uint16_t sensor1_mm = 0;
 	uint16_t sensor2_mm = 0;
 	const uint8_t numofsensors = 1;
 
-	  //GPIOD->BSRR = (uint32_t)GPIO_PIN_15 << 16U;
-	  GPIOD->BSRR = (uint32_t)GPIO_PIN_14 << 16U;
-	  HAL_Init();
-	  SystemClock_Config();
-	  MX_GPIO_Init();
-	  MX_I2C3_Init();
-	  //HAL_GPIO_WritePin(GPIOD,GPIO_PIN_15,GPIO_PIN_RESET);
-	  HAL_GPIO_WritePin(GPIOD,GPIO_PIN_14,GPIO_PIN_RESET);
-	  //memory allocation for VL53L0X_Dev_t structures and gpio pins
-	  VL53L0X_InitHandlerClass(numofsensors);
-	  //initialize VL53L0X_Dev_t structure, shutdown pin, comm type, speed, address
-	  //todo: return index number
-	  //VL53L0X_DefineAndPutDevice(GPIO_PIN_15, 1, 100, 0x60);
-	  HAL_Delay(500);
-	  VL53L0X_APPSConfigDevice(&hi2c3, GPIOD, GPIO_PIN_14, 1, 100, 0x22);
-	  HAL_Delay(500);
-	  //shutdown pins low state
-	  VL53L0X_APPSResetNodes();
-	  HAL_Delay(500);
-	  //set new address for 1st sensor
-	  //VL53L0X_ActiveNodeAddress(1);
-	  HAL_Delay(500);
-	  //initialize sensor with custom address
-	  //VL53L0X_APPStartCustomAddress(1);
-	  HAL_Delay(500);
-	  //set new address for 2nd sensor
-	  VL53L0X_APPSSetAddress(0);
-	  HAL_Delay(500);
-	  //initialize sensor with custom address
-	  VL53L0X_APPStartWithCustomAddress(0);
-	  HAL_Delay(500);
-
-	/********************End of C Code******************/
-	/***************************************************/
+	//HAL_GPIO_WritePin(GPIOD,GPIO_PIN_15,GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, GPIO_PIN_RESET);
+	//memory allocation for VL53L0X_Dev_t structures and gpio pins
+	VL53L0X_InitHandlerClass(numofsensors);
+	//initialize VL53L0X_Dev_t structure, shutdown pin, comm type, speed, address
+	//todo: return index number
+	//VL53L0X_DefineAndPutDevice(GPIO_PIN_15, 1, 100, 0x60);
+	HAL_Delay(500);
+	VL53L0X_APPSConfigDevice(&hi2c3, GPIOD, GPIO_PIN_14, 1, 100, 0x22);
+	HAL_Delay(500);
+	//shutdown pins low state
+	VL53L0X_APPSResetNodes();
+	HAL_Delay(500);
+	//set new address for 1st sensor
+	//VL53L0X_ActiveNodeAddress(1);
+	HAL_Delay(500);
+	//initialize sensor with custom address
+	//VL53L0X_APPStartCustomAddress(1);
+	HAL_Delay(500);
+	//set new address for 2nd sensor
+	VL53L0X_APPSSetAddress(0);
+	HAL_Delay(500);
+	//initialize sensor with custom address
+	VL53L0X_APPStartWithCustomAddress(0);
+	HAL_Delay(500);
 
 	while (1) {
 		Led1.toggle();
@@ -227,48 +188,45 @@ int main(void) {
 }
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
-void SystemClock_Config(void)
-{
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+ * @brief System Clock Configuration
+ * @retval None
+ */
+void SystemClock_Config(void) {
+	RCC_OscInitTypeDef RCC_OscInitStruct = { 0 };
+	RCC_ClkInitTypeDef RCC_ClkInitStruct = { 0 };
 
-  /** Configure the main internal regulator output voltage
-  */
-  __HAL_RCC_PWR_CLK_ENABLE();
-  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE3);
+	/** Configure the main internal regulator output voltage
+	 */
+	__HAL_RCC_PWR_CLK_ENABLE();
+	__HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE3);
 
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-  RCC_OscInitStruct.PLL.PLLM = 4;
-  RCC_OscInitStruct.PLL.PLLN = 72;
-  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
-  RCC_OscInitStruct.PLL.PLLQ = 3;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
+	/** Initializes the RCC Oscillators according to the specified parameters
+	 * in the RCC_OscInitTypeDef structure.
+	 */
+	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+	RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+	RCC_OscInitStruct.PLL.PLLM = 4;
+	RCC_OscInitStruct.PLL.PLLN = 72;
+	RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+	RCC_OscInitStruct.PLL.PLLQ = 3;
+	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+		Error_Handler();
+	}
 
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+	/** Initializes the CPU, AHB and APB buses clocks
+	 */
+	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
+			| RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
-  {
-    Error_Handler();
-  }
+	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK) {
+		Error_Handler();
+	}
 }
 
 /* USER CODE BEGIN 4 */
@@ -276,39 +234,36 @@ void SystemClock_Config(void)
 /* USER CODE END 4 */
 
 /**
-  * @brief  Period elapsed callback in non blocking mode
-  * @note   This function is called  when TIM6 interrupt took place, inside
-  * HAL_TIM_IRQHandler(). It makes a direct call to HAL_IncTick() to increment
-  * a global variable "uwTick" used as application time base.
-  * @param  htim : TIM handle
-  * @retval None
-  */
-void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
-{
-  /* USER CODE BEGIN Callback 0 */
+ * @brief  Period elapsed callback in non blocking mode
+ * @note   This function is called  when TIM6 interrupt took place, inside
+ * HAL_TIM_IRQHandler(). It makes a direct call to HAL_IncTick() to increment
+ * a global variable "uwTick" used as application time base.
+ * @param  htim : TIM handle
+ * @retval None
+ */
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
+	/* USER CODE BEGIN Callback 0 */
 
-  /* USER CODE END Callback 0 */
-  if (htim->Instance == TIM6) {
-    HAL_IncTick();
-  }
-  /* USER CODE BEGIN Callback 1 */
+	/* USER CODE END Callback 0 */
+	if (htim->Instance == TIM6) {
+		HAL_IncTick();
+	}
+	/* USER CODE BEGIN Callback 1 */
 
-  /* USER CODE END Callback 1 */
+	/* USER CODE END Callback 1 */
 }
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
-void Error_Handler(void)
-{
-  /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
-  __disable_irq();
-  while (1)
-  {
-  }
-  /* USER CODE END Error_Handler_Debug */
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
+void Error_Handler(void) {
+	/* USER CODE BEGIN Error_Handler_Debug */
+	/* User can add his own implementation to report the HAL error return state */
+	__disable_irq();
+	while (1) {
+	}
+	/* USER CODE END Error_Handler_Debug */
 }
 
 #ifdef  USE_FULL_ASSERT
