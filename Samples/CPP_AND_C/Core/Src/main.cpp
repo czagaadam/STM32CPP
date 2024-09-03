2b3c5dc57b4287187fd74d0fff95e009103e6607
czagaadam <czagaadam@gmail.com>
2024-09-03 20:03:12 +0200

    Remove comments.

--- Samples/CPP_AND_C/Core/Src/main.cpp
+++ Samples/CPP_AND_C/Core/Src/main.cpp
@@ -71,77 +71,19 @@ void HAL_I2C_MasterTxCpltCallback(I2C_HandleTypeDef *hi2c) {
 	}
 }
 
-/* Private includes ----------------------------------------------------------*/
-/* USER CODE BEGIN Includes */
-
-/* USER CODE END Includes */
-
-/* Private typedef -----------------------------------------------------------*/
-/* USER CODE BEGIN PTD */
-
-/* USER CODE END PTD */
-
-/* Private define ------------------------------------------------------------*/
-/* USER CODE BEGIN PD */
-
-/* USER CODE END PD */
-
-/* Private macro -------------------------------------------------------------*/
-/* USER CODE BEGIN PM */
-
-/* USER CODE END PM */
-
-/* Private variables ---------------------------------------------------------*/
-
-/* USER CODE BEGIN PV */
-
-/* USER CODE END PV */
-
-/* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
-/* USER CODE BEGIN PFP */
 
-/* USER CODE END PFP */
-
-/* Private user code ---------------------------------------------------------*/
-/* USER CODE BEGIN 0 */
-
-/* USER CODE END 0 */
-
-/**
- * @brief  The application entry point.
- * @retval int
- */
 int main(void) {
-
-	/* USER CODE BEGIN 1 */
-
-	/* USER CODE END 1 */
-
-	/* MCU Configuration--------------------------------------------------------*/
-
-	/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
 
 	//GPIOD->BSRR = (uint32_t)GPIO_PIN_15 << 16U;
 	GPIOD->BSRR = (uint32_t) GPIO_PIN_14 << 16U;
 	HAL_Init();
 
-	/* USER CODE BEGIN Init */
-
-	/* USER CODE END Init */
-
-	/* Configure the system clock */
 	SystemClock_Config();
 
-	/* USER CODE BEGIN SysInit */
-
-	/* USER CODE END SysInit */
-
-	/* Initialize all configured peripherals */
 	MX_GPIO_Init();
 	MX_I2C3_Init();
 	MX_UART5_Init();
-	/* USER CODE BEGIN 2 */
 
 	Led1 = Dout(GPIOD, GPIO_PIN_9, GPIO_PIN_RESET);
 	Btn1 = Din(GPIOD, GPIO_PIN_10);
@@ -184,7 +126,6 @@ int main(void) {
 		HAL_Delay(sensor1_mm);
 		sensor1_mm = VL53L0X_APPSNodeMeasure(0);
 	}
-	/* USER CODE END 3 */
 }
 
 /**
